290c83dcad7b8eb67345ea163d8c99ddada2db26
unknown <Sidd.c@Sidd-ADSC.ILLINOIS-SINGAPORE.LOCAL>
2015-08-05 16:01:19 +0800

    Working copy for lollipop

--- WallpaperChangerVersion2/app/src/main/java/com/siddhanta/wallpaperchangerversion2/MainActivity.java
+++ WallpaperChangerVersion2/app/src/main/java/com/siddhanta/wallpaperchangerversion2/MainActivity.java
@@ -2,17 +2,53 @@ package com.siddhanta.wallpaperchangerversion2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toolbar;
 
 
 public class MainActivity extends Activity {
+
+    Toolbar toolbar;
+    ViewPager pager;
+    ViewPagerAdapter adapter;
+    SlidingTabLayout tabs;
+    CharSequence Titles[] = {"Home","Events"};
+    int NumOfTabs = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitle("Wallpaper Changer");
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setActionBar(toolbar);
+
+        adapter = new ViewPagerAdapter(getFragmentManager(),Titles, NumOfTabs);
+
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+
+        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        tabs.setDistributeEvenly(true);
+
+        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.tabsScrollColor);
+            }
+        });
+
+        Log.d("WallpaperChanger", "Before set view pager");
+        tabs.setViewPager(pager);
+        Log.d("WallpaperChanger", "After set view pager");
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- WallpaperChangerVersion2/app/src/main/java/com/siddhanta/wallpaperchangerversion2/ViewPagerAdapter.java
+++ WallpaperChangerVersion2/app/src/main/java/com/siddhanta/wallpaperchangerversion2/ViewPagerAdapter.java
@@ -1,12 +1,46 @@
 package com.siddhanta.wallpaperchangerversion2;
 
 
-import android.support.v4.app.FragmentPagerAdapter;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.v13.app.FragmentPagerAdapter;
 
 /**
  * Created by Sidd.c on 5/8/2015.
  */
 public class ViewPagerAdapter extends FragmentPagerAdapter {
 
+    CharSequence Titles[];
+    int NumOfTabs;
 
+    public  ViewPagerAdapter (FragmentManager fm, CharSequence mTitles[], int mNumOfTabs) {
+        super(fm);
+
+        this.Titles=mTitles;
+        this.NumOfTabs=mNumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem (int position) {
+        if(position == 0)
+        {
+            Tab1 tab1 = new Tab1();
+            return tab1;
+        }
+        else
+        {
+            Tab2 tab2 = new Tab2();
+            return tab2;
+        }
+    }
+
+    @Override
+    public  CharSequence getPageTitle(int position){
+        return Titles[position];
+    }
+
+    @Override
+    public int getCount(){
+        return NumOfTabs;
+    }
 }
