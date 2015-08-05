626008ce6fd7164e3159ef19b357ff46bcf4549a
unknown <Sidd.c@Sidd-ADSC.ILLINOIS-SINGAPORE.LOCAL>
2015-08-05 13:51:13 +0800

    Basic things in place. Pager Adapter and main activity remaining

---
+++ WallpaperChangerVersion2/app/src/main/java/com/siddhanta/wallpaperchangerversion2/Tab1.java
@@ -0,0 +1,19 @@
+package com.siddhanta.wallpaperchangerversion2;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Sidd.c on 5/8/2015.
+ */
+public class Tab1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.tab_1, container, false);
+        return v;
+    }
+}
---
+++ WallpaperChangerVersion2/app/src/main/java/com/siddhanta/wallpaperchangerversion2/ViewPagerAdapter.java
@@ -0,0 +1,12 @@
+package com.siddhanta.wallpaperchangerversion2;
+
+
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Sidd.c on 5/8/2015.
+ */
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+
+}
